7689fc676cd6ef8c9cb26b3ceef8a51c95238d11
Bhagya Kavati <56948344+kavatibhagya@users.noreply.github.com>
2020-09-24 22:10:08 -0700

    Added to github pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  pathPrefix: "/portfolio",
   plugins: [
     `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
@@ -22,8 +23,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Simplefolio`,
-        short_name: `Simplefolio`,
+        name: `Bhagya portfolio`,
+        short_name: `Bhagya`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#02aab0`,
